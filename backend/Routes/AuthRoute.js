b7a53734ef34b09d1704929d0421bcc5e66cabeb
PriyankaPWagh <Priyankapw1994@gmail.com>
2024-11-27 17:41:28 +0530

    authroute

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -3,10 +3,10 @@ const { Holding } = require("../Controllers/Holding");
 const router = require("express").Router();
 const {userVerification}= require("../Middlewares/AuthMiddleware")
 
-router.post("https://zerodha-backend-wn62.onrender.com/signup", Signup);
-router.post("https://zerodha-backend-wn62.onrender.com/login", Login);
+router.post("/signup", Signup);
+router.post("/login", Login);
 //router router.get("/Holding",Holding)
-router.post("https://zerodha-backend-wn62.onrender.com", userVerification, (req, res) => {
+router.post("/", userVerification, (req, res) => {
     res.json({
         status: true,
         user: req.username, // or username, depending on your middleware
